5fe375700bc9b958ee3a1d67802b2458f7d0bcd2
unknown <jattinjaggi2002@gmail.com>
2024-06-17 19:13:12 +0530

    first commit

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,10 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import investment_pic from '../assests/investment-calculator-logo.png';
 
 function Header() {
     return (
         <div id='header'>
-            <img src={investment_pic} alt='Money Bag Picture'></img>
+            <img src={investment_pic} alt='Money Bag'></img>
             <h1>Investment Calculator</h1>
         </div>
     )
--- src/Components/SipType.js
+++ src/Components/SipType.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 
 function SipType({sip, changeSip}) {
 
